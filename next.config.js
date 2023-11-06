6f84c1260319d9c467b3fd7e7740c91a941e1b46
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-07 03:06:18 +0700

    chore: Comment out remotePatterns in images configuration.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['tecdn.b-cdn.net'],
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'tecdn.b-cdn.net',
+    //   }
+    // ]
   }
 }
 
