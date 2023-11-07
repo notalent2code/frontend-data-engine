2193968ec232c486c5b3ff1d2d95aea578779314
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-07 11:54:40 +0700

    feat: Add icons and links to user session dropdown menu

--- src/components/UserSessionNav.tsx
+++ src/components/UserSessionNav.tsx
@@ -13,6 +13,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/DropdownMenu';
 import { useRouter } from 'next/navigation';
+import { Home, LayoutDashboard, LogOut, Trophy } from 'lucide-react';
 
 export function UserSessionNav() {
   const router = useRouter();
@@ -29,40 +30,55 @@ export function UserSessionNav() {
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar>
-          <span className='sr-only'>{user.first_name}</span>
-          <Icons.user className='w-8 h-8 text-primary' />
+          <div className='flex flex-row justify-between items-center gap-2'>
+            <span className='font-semibold text-sm'>
+              {`${user.first_name} ${user.last_name}`}
+            </span>
+            <Icons.user className='w-8 h-8 text-primary' />
+          </div>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className='bg-white' align='end'>
-        <div className='flex items-center justify-start gap-2 p-2'>
-          <div className='flex flex-col space-y-1 leading-none'>
+      <DropdownMenuContent align='end'>
+        <div className='flex flex-col items-center justify-start gap-2 p-2'>
+          <div className='space-y-1 leading-none'>
             <p className='font-medium'>{`${user.first_name} (${user.role})`}</p>
-            <p className='w-[200px] truncate text-sm text-primary'>
-              {user.email}
-            </p>
+            <p className='truncate text-sm text-primary'>{user.email}</p>
           </div>
         </div>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild className='cursor-pointer'>
+
+        <DropdownMenuSeparator/>
+        
+        <DropdownMenuItem className='cursor-pointer'>
+          <Home className='mr-2 h-4 w-4' />
           <Link href='/'>Home</Link>
         </DropdownMenuItem>
 
-        <DropdownMenuItem asChild className='cursor-pointer'>
+        <DropdownMenuItem className='cursor-pointer'>
+          <LayoutDashboard className='mr-2 h-4 w-4' />
           <Link href='/dashboard'>Dashboard</Link>
         </DropdownMenuItem>
 
-        <DropdownMenuItem asChild className='cursor-pointer'>
+        <DropdownMenuItem className='cursor-pointer'>
+          <Trophy className='mr-2 h-4 w-4' />
           <Link href='/portfolio'>Portfolio</Link>
         </DropdownMenuItem>
+
         <DropdownMenuSeparator />
+       
         <DropdownMenuItem className='cursor-pointer' onSelect={() => logout()}>
-          Log out
+          <LogOut className='mr-2 h-4 w-4' />
+          <p>Log Out</p>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   ) : (
-    <Link href='/auth/login' className={buttonVariants()}>
-      Log In
-    </Link>
+    <>
+      <Link href='/portfolio' className='text-md font-bold text-primary'>
+        Portfolio
+      </Link>
+      <Link href='/auth/login' className={buttonVariants()}>
+        Log In
+      </Link>
+    </>
   );
 }
