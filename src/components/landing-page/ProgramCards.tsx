98bc733e4caac30100da06c740ac6de2596121ff
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-07 12:10:47 +0700

    chore: Remove unused import in ProgramCards.tsx

--- src/components/landing-page/ProgramCards.tsx
+++ src/components/landing-page/ProgramCards.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/Card';
+import { Card, CardContent } from '@/components/ui/Card';
 import Image from 'next/image';
 
 type Program = {
