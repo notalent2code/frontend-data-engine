6a9c14c95c594804e2a98d8379f4b9fc7d9593bc
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-08 19:51:56 +0700

    feat: Add reset password page with link to home

---
+++ src/app/auth/reset-password/page.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import { FC } from 'react';
+import { ChevronLeft } from 'lucide-react';
+import { buttonVariants } from '@/components/ui/Button';
+import { cn } from '@/lib/utils';
+import ResetPassword from '@/components/auth/ResetPassword';
+
+const Page: FC = () => {
+  return (
+    <div className='absolute inset-0'>
+      <div className='h-full max-w-2xl mx-auto mt-10 flex flex-col items-center justify-center gap-5'>
+        <Link
+          href='/'
+          className={cn(
+            buttonVariants({ variant: 'ghost' }),
+            'self-start -mt-20'
+          )}
+        >
+          <ChevronLeft className='mr-2 h-4 w-4' />
+          Home
+        </Link>
+
+        <ResetPassword />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
