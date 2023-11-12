0940ba9065f130de54cfa73705136d793e866d3d
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-13 06:00:11 +0700

    chore: Update remotePatterns in images configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'tecdn.b-cdn.net',
-    //   }
-    // ]
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'indigo-data-engine.s3.ap-southeast-3.amazonaws.com',
+      }
+    ]
   }
 }
 
