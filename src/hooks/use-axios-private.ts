8897c202c9621cd1c701405a6ead9bb044ab9e28
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-12 16:51:00 +0700

    feat: Refactor useAxiosPrivate to use authStore token.

--- src/hooks/use-axios-private.ts
+++ src/hooks/use-axios-private.ts
@@ -1,21 +1,23 @@
 import { useEffect } from 'react';
 import axios from '@/lib/axios';
 import useRefreshtoken from '@/hooks/use-refresh-token';
+import { useAuthStore } from '@/store/auth-store';
 
 const useAxiosPrivate = () => {
+  const authStore = useAuthStore();
   const { refresh } = useRefreshtoken();
 
   useEffect(() => {
-    // const requestIntercept = axios.interceptors.request.use(
-    //   (config) => {
-    //     if (!config.headers.Authorization) {
-    //       config.headers.Authorization = `Bearer ${localStorage.getItem('access_token')}`;
-    //     }
-    //     return config;
-    //   }, (error) => {
-    //     Promise.reject(error);
-    //   }
-    // )
+    const requestIntercept = axios.interceptors.request.use(
+      (config) => {
+        if (!config.headers.Authorization) {
+          config.headers.Authorization = `Bearer ${authStore.token}`;
+        }
+        return config;
+      }, (error) => {
+        Promise.reject(error);
+      }
+    )
 
     const responseIntercept = axios.interceptors.response.use(
       (response) => response,
@@ -24,6 +26,9 @@ const useAxiosPrivate = () => {
         if (error.response.status === 401 && !originalRequest.sent) {
           originalRequest.sent = true;
           const { access_token } = await refresh();
+          // eslint-disable-next-line no-console
+          console.log('Refresh token', access_token)
+          authStore.setToken(access_token);
           originalRequest.headers.Authorization = `Bearer ${access_token}`;
           return axios(originalRequest)
         }
@@ -32,10 +37,10 @@ const useAxiosPrivate = () => {
     );
 
     return () => {
-      // axios.interceptors.request.eject(requestIntercept);
+      axios.interceptors.request.eject(requestIntercept);
       axios.interceptors.response.eject(responseIntercept);
     };
-  }, [refresh]);
+  }, [authStore, refresh]);
 
   return axios;
 };
