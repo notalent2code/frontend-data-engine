6695a1b827b2da50a96916b4992b5dacf676a786
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-12 16:50:42 +0700

    feat: Add token functionality to auth store

--- src/store/auth-store.ts
+++ src/store/auth-store.ts
@@ -5,16 +5,22 @@ import { UserSession } from '@/types/user-session';
 
 type AuthStore = {
   session: UserSession | undefined;
+  token: string | undefined;
   setSession: (session: UserSession) => void;
   deleteSession: () => void;
+  setToken: (token: string) => void;
+  deleteToken: () => void;
 };
 
 export const useAuthStore = create<AuthStore>()(
   persist(
     (set) => ({
       session: undefined,
+      token: undefined,
       setSession: (session) => set({ session }),
       deleteSession: () => set({ session: undefined }),
+      setToken: (token) => set({ token }),
+      deleteToken: () => set({ token: undefined }),
     }),
     { name: 'auth-store' }
   )
