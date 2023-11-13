e98bec6ddf2d96e8ddaecbdb4e7b90848c408b51
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-13 23:48:09 +0700

    Add search icon to search input

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,21 @@
 import { Input } from '@/components/ui/Input';
+import { SearchIcon } from 'lucide-react';
 import { FC } from 'react';
 
 interface SearchProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Search: FC<SearchProps> = ({ ...props }) => {
   return (
-    <Input
-      {...props}
-      type='search'
-      placeholder='Search...'
-      className='w-[200px] md:w-[300px] bg-white my-6'
-    />
+    <div className='relative flex flex-row items-center w-[200px] md:w-[300px] my-6'>
+      <Input
+        {...props}
+        name='search'
+        type='search'
+        placeholder='Search...'
+        className='w-full bg-white pl-8'
+      />
+      <SearchIcon className='absolute left-2 pr-1 text-muted-foreground' />{' '}
+    </div>
   );
 };
 
