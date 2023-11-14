9b1d063beaf8ce20a0bac11881729010e35e64fb
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-14 20:17:22 +0700

    Add sidebar store with toggle functionality

---
+++ src/store/sidebar-store.ts
@@ -0,0 +1,11 @@
+import { create } from 'zustand';
+
+type SidebarStore = {
+  isSidebarOpen: boolean;
+  toggleSidebar: () => void;
+};
+
+export const useSidebarStore = create<SidebarStore>((set) => ({
+  isSidebarOpen: false,
+  toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
+}));
