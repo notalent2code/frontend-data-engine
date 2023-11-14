eccafab723211f3698ac6eeb25ebe3f47cf3cf7c
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-14 20:18:49 +0700

    Add Navbar component and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,17 @@ import Program from '@/components/landing-page/Program';
 import Benefit from '@/components/landing-page/Benefit';
 import Summary from '@/components/landing-page/Summary';
 import Footer from '@/components/landing-page/Footer';
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
     <div className='flex flex-col gap-12 pt-12'>
-      <Hero className='p-8' id='hero' />
+      <Navbar logo />
+      <Hero className='p-8 main-bg' />
       <Program />
       <Summary className='p-8 bg-gradient-radial from-slate-100 to-zinc-200' />
       <Benefit />
-      <Footer className='p-8' id='footer' />
+      <Footer className='p-8 main-bg' />
     </div>
   );
 }
