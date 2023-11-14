d370b44ddbcec1b9c0042fe1922e5a37f6be171d
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-15 04:53:58 +0700

    Update layout padding in dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,29 +1,19 @@
-'use client';
-
 import '@/styles/globals.css';
 import Sidebar from '@/components/Sidebar';
 import Navbar from '@/components/Navbar';
-import { useRouter } from 'next/navigation';
-import { useAuthStore } from '@/store/auth-store';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-  const user = useAuthStore((state) => state.session);
-
-  if (!user) {
-    router.push('/auth/login');
-  }
 
   return (
     <div className='flex'>
       <Sidebar />
       <main className='flex-1'>
         <Navbar logo={false} />
-        <div className='container mx-auto p-8 pt-16'>{children}</div>
+        <div className='container mx-auto p-8 pt-20'>{children}</div>
       </main>
     </div>
   );
