e3d747760bb29cdf3386bc33541e3f5bff46ed3e
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-15 04:53:50 +0700

    Add summary cards and startup sectors to analytics page

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import StartupSectors from '@/components/dashboard/StartupSectors';
+import SummaryCards from '@/components/landing-page/SummaryCards';
+import { Card } from '@/components/ui/Card';
 
 const Page = () => {
   return (
-    <div>Analytics Page</div>
-  )
-}
+    <div className='grid grid-flow-row gap-8'>
+      <SummaryCards type={'dashboard'} />
+      <div className='flex flex-row items-center'>
+        <StartupSectors />
+        <Card className='ml-8'>
+          <div className='text-2xl font-semibold'>Startup Stages</div>
+          <div className='text-4xl font-bold'>5</div>
+        </Card>
+      </div>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
