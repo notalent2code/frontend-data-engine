889c137af9714aee20ecc6bb2f03f39fca6259e0
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-14 22:33:48 +0700

    Refactor authentication pages to use shared components

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -7,21 +7,19 @@ import Login from '@/components/auth/Login';
 
 const Page: FC = () => {
   return (
-    <div className='absolute inset-0'>
-      <div className='h-full max-w-2xl mx-auto mt-10 flex flex-col items-center justify-center gap-5'>
-        <Link
-          href='/'
-          className={cn(
-            buttonVariants({ variant: 'ghost' }),
-            'self-start -mt-20'
-          )}
-        >
-          <ChevronLeft className='mr-2 h-4 w-4' />
-          Home
-        </Link>
+    <div className='h-full max-w-2xl mx-auto mt-10 flex flex-col items-center justify-center gap-5'>
+      <Link
+        href='/'
+        className={cn(
+          buttonVariants({ variant: 'ghost' }),
+          'self-start -mt-20'
+        )}
+      >
+        <ChevronLeft className='mr-2 h-4 w-4' />
+        Home
+      </Link>
 
-        <Login />
-      </div>
+      <Login />
     </div>
   );
 };
--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -7,21 +7,19 @@ import ResetPassword from '@/components/auth/ResetPassword';
 
 const Page: FC = () => {
   return (
-    <div className='absolute inset-0'>
-      <div className='h-full max-w-2xl mx-auto mt-10 flex flex-col items-center justify-center gap-5'>
-        <Link
-          href='/'
-          className={cn(
-            buttonVariants({ variant: 'ghost' }),
-            'self-start -mt-20'
-          )}
-        >
-          <ChevronLeft className='mr-2 h-4 w-4' />
-          Home
-        </Link>
+    <div className='h-full max-w-2xl mx-auto mt-10 flex flex-col items-center justify-center gap-5'>
+      <Link
+        href='/'
+        className={cn(
+          buttonVariants({ variant: 'ghost' }),
+          'self-start -mt-20'
+        )}
+      >
+        <ChevronLeft className='mr-2 h-4 w-4' />
+        Home
+      </Link>
 
-        <ResetPassword />
-      </div>
+      <ResetPassword />
     </div>
   );
 };
