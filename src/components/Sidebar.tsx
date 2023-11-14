e92c3d96cd5db08204d0c78bcdae35273ee92c87
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-15 04:53:42 +0700

    Update Sidebar component to justify-start NavigationLinks

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,7 +5,7 @@ import NavigationLinks from '@/components/NavigationLinks';
 const Sidebar = () => {
   return (
     <aside className='sticky top-0 z-10 hidden md:block h-screen w-56 p-4 main-bg'>
-      <div className='flex items-center mb-4 space-x-1 pl-2'>
+      <div className='flex items-center mb-4 justify-start space-x-1 pl-2'>
         <Link href='/dashboard/analytics'>
           <Icons.whiteLogo />
         </Link>
