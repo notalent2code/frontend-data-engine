f38ea28664bfbbbf2801d2b87fd01ca888aeeea5
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-15 06:03:37 +0700

    Add GameStages component and remove Card component from dashboard analytics page

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -1,17 +1,14 @@
+import GameStages from '@/components/dashboard/GameStages';
 import StartupSectors from '@/components/dashboard/StartupSectors';
 import SummaryCards from '@/components/landing-page/SummaryCards';
-import { Card } from '@/components/ui/Card';
 
 const Page = () => {
   return (
     <div className='grid grid-flow-row gap-8'>
       <SummaryCards type={'dashboard'} />
-      <div className='flex flex-row items-center'>
+      <div className='flex flex-row items-center justify-between'>
         <StartupSectors />
-        <Card className='ml-8'>
-          <div className='text-2xl font-semibold'>Startup Stages</div>
-          <div className='text-4xl font-bold'>5</div>
-        </Card>
+        <GameStages />
       </div>
     </div>
   );
