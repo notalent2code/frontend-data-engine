48c1afda11b793403a3d5bcbcc6c528db92b9568
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-15 22:29:24 +0700

    Update layout of dashboard analytics page

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -6,7 +6,8 @@ const Page = () => {
   return (
     <div className='grid grid-flow-row gap-8'>
       <SummaryCards type={'dashboard'} />
-      <div className='flex flex-col gap-4 lg:flex-row items-center md:items-start justify-start md:justify-between'>
+      <div className='flex flex-col gap-8 lg:flex-row items-center md:items-start justify-start md:justify-between'>
+      {/* <div className='grid grid-flow-col gap-4 w-full h'> */}
         <StartupSectors />
         <GameStages />
       </div>
