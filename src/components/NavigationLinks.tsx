a32300a08340cf8ddbcc689c662d66f149b26b25
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-15 15:46:49 +0700

    fix: Add Loader component and use useEffect to set user role

--- src/components/NavigationLinks.tsx
+++ src/components/NavigationLinks.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import { BarChart2, Landmark, LucideIcon, Users, Wallet2 } from 'lucide-react';
 import { Role } from '@prisma/client';
 import { useAuthStore } from '@/store/auth-store';
+import { Loader } from './ui/Loader';
+import { useEffect, useState } from 'react';
 
 type NavigationLink = {
   name: string;
@@ -48,31 +50,40 @@ export const investorLinks: NavigationLink[] = [
 ];
 
 const NavigationLinks = () => {
-  const role = useAuthStore((state) => state.session?.role);
+  const [role, setRole] = useState<Role | undefined>(undefined);
+  const userRole = useAuthStore((state) => state.session?.role);
 
-  return role === Role.ADMIN
-    ? adminLinks.map((link) => (
-        <Link href={link.path} key={link.name}>
-          <button
-            className='w-full flex items-center space-x-2 bg-slate-800 bg-opacity-10 
+  useEffect(() => {
+    setRole(userRole);
+  }, [userRole]);
+
+  return role === Role.ADMIN ? (
+    adminLinks.map((link) => (
+      <Link href={link.path} key={link.name}>
+        <button
+          className='w-full flex items-center space-x-2 bg-slate-800 bg-opacity-10 
           hover:bg-opacity-30 active:bg-opacity-30 py-2 px-2 my-2 rounded-lg text-white'
-          >
-            <link.icon className='w-4 h-4' />
-            <span className='text-sm font-semibold'>{link.name}</span>
-          </button>
-        </Link>
-      ))
-    : investorLinks.map((link) => (
-        <Link href={link.path} key={link.name}>
-          <button
-            className='w-full flex items-center space-x-2 bg-slate-800 bg-opacity-10 
+        >
+          <link.icon className='w-4 h-4' />
+          <span className='text-sm font-semibold'>{link.name}</span>
+        </button>
+      </Link>
+    ))
+  ) : role === Role.INVESTOR ? (
+    investorLinks.map((link) => (
+      <Link href={link.path} key={link.name}>
+        <button
+          className='w-full flex items-center space-x-2 bg-slate-800 bg-opacity-10 
           hover:bg-opacity-30 active:bg-opacity-30 py-2 px-2 my-2 rounded-lg text-white'
-          >
-            <link.icon className='w-4 h-4' />
-            <span className='text-sm font-semibold'>{link.name}</span>
-          </button>
-        </Link>
-      ));
+        >
+          <link.icon className='w-4 h-4' />
+          <span className='text-sm font-semibold'>{link.name}</span>
+        </button>
+      </Link>
+    ))
+  ) : (
+    <Loader />
+  );
 };
 
 export default NavigationLinks;
