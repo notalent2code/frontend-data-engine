a74c32337cf8893b41227c79a4c71f40798ca1bb
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-16 03:19:09 +0700

    Add revenue summary components to dashboard page

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -1,16 +1,21 @@
 import GameStages from '@/components/dashboard/GameStages';
+import RevenueSummary from '@/components/dashboard/RevenueSummary';
 import StartupSectors from '@/components/dashboard/StartupSectors';
+import TopRevenue from '@/components/dashboard/TopRevenue';
 import SummaryCards from '@/components/landing-page/SummaryCards';
 
 const Page = () => {
   return (
     <div className='grid grid-flow-row gap-8'>
       <SummaryCards type={'dashboard'} />
-      <div className='flex flex-col gap-8 lg:flex-row items-center md:items-start justify-start md:justify-between'>
-      {/* <div className='grid grid-flow-col gap-4 w-full h'> */}
+      <div className='grid grid-cols-1 md:grid-cols-[2fr,1fr] gap-8 items-center md:items-start justify-start md:justify-between'>
         <StartupSectors />
         <GameStages />
       </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+        <TopRevenue />
+      </div>
+      <RevenueSummary />
     </div>
   );
 };
