d2477aab10dbf9a2a63ce173020d99ceef33ca3d
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-18 18:37:40 +0700

    Refactor Heading component to accept additional props

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -1,13 +1,13 @@
-interface HeadingProps {
+interface HeadingProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string;
   description: string;
 }
 
-export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
+export const Heading: React.FC<HeadingProps> = ({ title, description, ...props }) => {
   return (
-    <div>
+    <div {...props}>
       <h2 className='text-3xl font-bold tracking-tight'>{title}</h2>
-      <p className='text-sm text-muted-foreground pt-2'>{description}</p>
+      <p className='text-sm text-muted-foreground py-2'>{description}</p>
     </div>
   );
 };
