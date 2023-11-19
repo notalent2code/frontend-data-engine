5504fbc3bb03b9e125bb654cd0229d6383a4e74c
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-19 16:46:03 +0700

    Add enumReplacer and parseSynergyConfidenceLevel functions

--- src/util/enum-replacer.ts
+++ src/util/enum-replacer.ts
@@ -1,3 +1,22 @@
+import { SynergyConfidenceLevel } from "@prisma/client";
+
 export const enumReplacer = (value: string) => {
   return value.replace(/_/g, ' ').toUpperCase();
+}
+
+export const parseSynergyConfidenceLevel = (value: SynergyConfidenceLevel) => {
+  switch (value) {
+    case SynergyConfidenceLevel.PERCENT_0_TO_20:
+      return '0 - 20 %';
+    case SynergyConfidenceLevel.PERCENT_21_TO_40:
+      return '21 - 40 %';
+    case SynergyConfidenceLevel.PERCENT_41_TO_60:
+      return '41 - 60 %';
+    case SynergyConfidenceLevel.PERCENT_61_TO_80:
+      return '61 - 80 %';
+    case SynergyConfidenceLevel.PERCENT_81_TO_100:
+      return '81 - 100 %';
+    default:
+      return 'Unknown';
+  }
 }
