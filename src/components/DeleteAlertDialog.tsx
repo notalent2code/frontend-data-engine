524417ada1680c966a0b717defd275642827fdf0
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-28 01:49:56 +0700

    Refactor DeleteAlertDialog component

--- src/components/DeleteAlertDialog.tsx
+++ src/components/DeleteAlertDialog.tsx
@@ -10,8 +10,6 @@ import {
   AlertDialogTrigger,
 } from '@/components/ui/AlertDialog';
 import { FC } from 'react';
-import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/Button';
 
 interface DeleteAlertDialogProps extends React.HTMLAttributes<HTMLDivElement> {
   deleteFn: () => void;
@@ -19,26 +17,24 @@ interface DeleteAlertDialogProps extends React.HTMLAttributes<HTMLDivElement> {
 
 const DeleteAlertDialog: FC<DeleteAlertDialogProps> = ({ deleteFn }) => {
   return (
-    <div className={cn(buttonVariants({ size: 'lg' }))}>
-      <AlertDialog>
-        <AlertDialogTrigger>Delete</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              Please be aware that this action is irreversible. Once completed,
-              the data will be permanently erased and cannot be retrieved.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction onClick={() => deleteFn()}>
-              Continue
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </div>
+    <AlertDialog>
+      <AlertDialogTrigger>Delete</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Please be aware that this action is irreversible. Once completed,
+            the data will be permanently erased and cannot be retrieved.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={() => deleteFn()}>
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
