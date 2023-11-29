b90b22d9b6519bf569939ee207ec0a9afb984420
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-29 07:24:28 +0700

    Update ACCEPTED_IMAGE_TYPES array in constants file

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 const MAX_FILE_SIZE = 1024 * 1024 * 5;
-const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", 'image/webp'];
+const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png'];
 
 export { MAX_FILE_SIZE, ACCEPTED_IMAGE_TYPES };
