24e780a729856e371f64bb36e0d206a7cd3e12b8
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-29 07:27:03 +0700

    Add map click functionality

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,15 +4,21 @@ import { FC, useCallback, useState } from 'react';
 interface MapProps {
   latitude: number;
   longitude: number;
+  // eslint-disable-next-line no-unused-vars
+  onMapClick?: (latLng: { lat: number; lng: number }) => void;
 }
 
-const Map: FC<MapProps> = ({ latitude, longitude }) => {
+const Map: FC<MapProps> = ({ latitude, longitude, onMapClick }) => {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
   });
 
   const [map, setMap] = useState(null);
+  const [clickedLatLng, setClickedLatLng] = useState<{
+    lat: number | null;
+    lng: number | null;
+  }>({ lat: null, lng: null });
 
   const onLoad = useCallback(() => {
     setMap(map);
@@ -22,6 +28,18 @@ const Map: FC<MapProps> = ({ latitude, longitude }) => {
     setMap(null);
   }, []);
 
+  const handleMapClick = useCallback(
+    (e: google.maps.MapMouseEvent) => {
+      const lat = e.latLng?.lat();
+      const lng = e.latLng?.lng();
+      if (lat && lng) {
+        setClickedLatLng({ lat, lng });
+        onMapClick?.({ lat, lng });
+      }
+    },
+    [onMapClick]
+  );
+
   const containerStyle = {
     width: '100%',
     height: '400px',
@@ -29,6 +47,7 @@ const Map: FC<MapProps> = ({ latitude, longitude }) => {
 
   return isLoaded ? (
     <GoogleMap
+      options={{}}
       mapContainerStyle={containerStyle}
       center={{
         lat: latitude,
@@ -37,11 +56,12 @@ const Map: FC<MapProps> = ({ latitude, longitude }) => {
       zoom={15}
       onLoad={onLoad}
       onUnmount={onUnmount}
+      onClick={handleMapClick}
     >
       <Marker
         position={{
-          lat: latitude,
-          lng: longitude,
+          lat: clickedLatLng.lat || latitude,
+          lng: clickedLatLng.lng || longitude,
         }}
       />
     </GoogleMap>
