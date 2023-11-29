c9874ceb857eb1fae757e4b7462b1d01a7846f76
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-11-29 07:46:48 +0700

    Add image-preview utility function

---
+++ src/util/image-preview.ts
@@ -0,0 +1,12 @@
+export const getImageData = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const dataTransfer = new DataTransfer();
+
+  Array.from(event.target.files!).forEach((image) =>
+    dataTransfer.items.add(image as File)
+  );
+
+  const files = dataTransfer.files;
+  const displayUrl = URL.createObjectURL(event.target.files![0]);
+
+  return { file: files[0], displayUrl };
+}
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,4 @@
 export * from './format-rupiah';
 export * from './enum-replacer';
 export * from './array-parser';
+export * from './image-preview';
