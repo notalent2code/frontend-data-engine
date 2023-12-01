c4d345e69bf77a8bbf67540b5d4120325ddcf976
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-12-02 03:41:39 +0700

    Refactor synergy component

--- src/components/startup/Synergy.tsx
+++ src/components/startup/Synergy.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { FC } from 'react';
 import Link from 'next/link';
 import dayjs from 'dayjs';
@@ -7,7 +9,6 @@ import { Card } from '@/components/ui/Card';
 import { Heading } from '@/components/ui/Heading';
 import { Separator } from '@/components/ui/Separator';
 import { buttonVariants } from '@/components/ui/Button';
-import DropdownActions from '@/components/ui/DropdownActions';
 import { enumReplacer, parseSynergyConfidenceLevel } from '@/util';
 import {
   Table,
@@ -17,15 +18,49 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/Table';
+import useAxiosPrivate from '@/hooks/use-axios-private';
+import { useAuthStore } from '@/store/auth-store';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/AlertDialog';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/Tooltip';
+import { Edit, Trash2 } from 'lucide-react';
+import toast from 'react-hot-toast';
 
 interface SynergyProps {
   data: Synergy[] | null;
-  addUrl: string;
-  editUrl: string;
-  deleteUrl: string;
+  baseUrl: string;
 }
 
-const Synergy: FC<SynergyProps> = ({ data, addUrl, editUrl, deleteUrl }) => {
+const Synergy: FC<SynergyProps> = ({ data, baseUrl }) => {
+  const axios = useAxiosPrivate();
+  const role = useAuthStore((state) => state.session?.role);
+
+  const deleteSynergy = async (id: string) => {
+    try {
+      await axios.delete(`/synergy/${id}`);
+      toast.success('Synergy deleted successfully!');
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    } catch (err) {
+      toast.error('Failed to delete synergy!');
+    }
+  };
+
   return (
     <div className='flex flex-col gap-2'>
       <div className='pb-4'>
@@ -34,12 +69,17 @@ const Synergy: FC<SynergyProps> = ({ data, addUrl, editUrl, deleteUrl }) => {
             title='Startup Partnership'
             description='Startup detail information about partnership with Telkom Indonesia.'
           />
-          <Link
-            href={addUrl + '/contract'}
-            className={cn(buttonVariants({ size: 'lg' }))}
-          >
-            Add new
-          </Link>
+          {role === 'ADMIN' && (
+            <Link
+              href={baseUrl + '/synergy/create'}
+              className={cn(
+                buttonVariants({ size: 'sm' }),
+                'bg-tertiary hover:bg-tertiary hover:opacity-90'
+              )}
+            >
+              Add new
+            </Link>
+          )}
         </div>
         <Separator className='mt-4 lg:mt-0' />
       </div>
@@ -50,46 +90,117 @@ const Synergy: FC<SynergyProps> = ({ data, addUrl, editUrl, deleteUrl }) => {
               <Table>
                 <TableHeader>
                   <TableRow>
-                    <TableHead className='p-5'>Telkom Group</TableHead>
-                    <TableHead>Entity</TableHead>
-                    <TableHead>Model</TableHead>
-                    <TableHead>Description</TableHead>
-                    <TableHead>Progress</TableHead>
-                    <TableHead>Lead Time Week</TableHead>
-                    <TableHead>Output</TableHead>
-                    <TableHead>Confidence Level</TableHead>
-                    <TableHead>Project Status</TableHead>
-                    <TableHead>Initiation Date</TableHead>
-                    <TableHead>Actions</TableHead>
+                    <TableHead className='px-4 py-2 text-xs'>
+                      Telkom Group
+                    </TableHead>
+                    <TableHead className='px-4 py-2 text-xs'>Entity</TableHead>
+                    <TableHead className='px-4 py-2 text-xs'>Model</TableHead>
+                    <TableHead className='px-4 py-2 text-xs'>
+                      Description
+                    </TableHead>
+                    <TableHead className='px-4 py-2 text-xs'>
+                      Progress
+                    </TableHead>
+                    <TableHead className='px-4 py-2 text-xs'>
+                      Lead Time Week
+                    </TableHead>
+                    <TableHead className='px-4 py-2 text-xs'>Output</TableHead>
+                    <TableHead className='px-4 py-2 text-xs'>
+                      Confidence Level
+                    </TableHead>
+                    <TableHead className='px-4 py-2 text-xs'>
+                      Project Status
+                    </TableHead>
+                    <TableHead className='px-4 py-2 text-xs'>
+                      Initiation Date
+                    </TableHead>
+                    {role === 'ADMIN' && (
+                      <TableHead className='px-4 py-2 text-xs'>
+                        Actions
+                      </TableHead>
+                    )}
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {data.map((synergy) => (
-                    <TableRow key={synergy.id}>
-                      <TableCell className='p-5'>
-                        {synergy.telkom_group}
+                  {data.map((item) => (
+                    <TableRow key={item.id}>
+                      <TableCell className='px-4 py-2 text-xs'>
+                        {item.telkom_group}
                       </TableCell>
-                      <TableCell>{synergy.entity}</TableCell>
-                      <TableCell>{enumReplacer(synergy.model)}</TableCell>
-                      <TableCell>{synergy.description}</TableCell>
-                      <TableCell>{enumReplacer(synergy.progress)}</TableCell>
-                      <TableCell>{synergy.lead_time_week}</TableCell>
-                      <TableCell>{enumReplacer(synergy.output)}</TableCell>
-                      <TableCell>
-                        {parseSynergyConfidenceLevel(synergy.confidence_level)}
+                      <TableCell className='px-4 py-2 text-xs'>
+                        {item.entity}
                       </TableCell>
-                      <TableCell>
-                        {enumReplacer(synergy.project_status)}
+                      <TableCell className='px-4 py-2 text-xs'>
+                        {enumReplacer(item.model)}
                       </TableCell>
-                      <TableCell>
-                        {dayjs(synergy.initiation_date).format('D MMMM YYYY')}
+                      <TableCell className='px-4 py-2 text-xs'>
+                        {item.description}
                       </TableCell>
-                      <TableCell>
-                        <DropdownActions
-                          editUrl={`${editUrl}/synergy/${synergy.id}`}
-                          deleteUrl={`${deleteUrl}/synergy/${synergy.id}`}
-                        />
+                      <TableCell className='px-4 py-2 text-xs'>
+                        {enumReplacer(item.progress)}
                       </TableCell>
+                      <TableCell className='px-4 py-2 text-xs'>
+                        {item.lead_time_week}
+                      </TableCell>
+                      <TableCell className='px-4 py-2 text-xs'>
+                        {enumReplacer(item.output)}
+                      </TableCell>
+                      <TableCell className='px-4 py-2 text-xs'>
+                        {parseSynergyConfidenceLevel(item.confidence_level)}
+                      </TableCell>
+                      <TableCell className='px-4 py-2 text-xs'>
+                        {enumReplacer(item.project_status)}
+                      </TableCell>
+                      <TableCell className='px-4 py-2 text-xs'>
+                        {dayjs(item.initiation_date).format('D MMMM YYYY')}
+                      </TableCell>
+                      {role === 'ADMIN' && (
+                        <TableCell className='px-4 py-2 text-xs'>
+                          <div className='flex flex-row gap-2'>
+                            <TooltipProvider>
+                              <Tooltip>
+                                <TooltipTrigger>
+                                  <Link
+                                    href={`${baseUrl}/synergy/${item.id}/edit`}
+                                  >
+                                    <Edit className='h-6 w-6' />
+                                  </Link>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                  <p>Edit data</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </TooltipProvider>
+
+                            <AlertDialog>
+                              <AlertDialogTrigger>
+                                <Trash2 className='h-6 w-6' />
+                              </AlertDialogTrigger>
+                              <AlertDialogContent>
+                                <AlertDialogHeader>
+                                  <AlertDialogTitle>
+                                    Are you absolutely sure?
+                                  </AlertDialogTitle>
+                                  <AlertDialogDescription>
+                                    Please be aware that this action is
+                                    irreversible. Once completed, the data will
+                                    be permanently erased and cannot be
+                                    retrieved.
+                                  </AlertDialogDescription>
+                                </AlertDialogHeader>
+                                <AlertDialogFooter>
+                                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                  <AlertDialogAction
+                                    onClick={() => deleteSynergy(item.id)}
+                                  >
+                                    Continue
+                                  </AlertDialogAction>
+                                </AlertDialogFooter>
+                              </AlertDialogContent>
+                            </AlertDialog>
+                          </div>
+                        </TableCell>
+                      )}
                     </TableRow>
                   ))}
                 </TableBody>
