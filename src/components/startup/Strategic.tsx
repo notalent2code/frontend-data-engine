14c002ec9c94ce49d25b78f29eb4dafed68afeff
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-12-02 05:13:30 +0700

    Refactor Strategic component to add delete functionality

--- src/components/startup/Strategic.tsx
+++ src/components/startup/Strategic.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { FC } from 'react';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
@@ -7,15 +9,49 @@ import { Heading } from '@/components/ui/Heading';
 import { Separator } from '@/components/ui/Separator';
 import { buttonVariants } from '@/components/ui/Button';
 import { enumReplacer } from '@/util';
-import { BarChart2, Briefcase, Users2 } from 'lucide-react';
+import { BarChart2, Briefcase, Edit, Trash2, Users2 } from 'lucide-react';
+import { useAuthStore } from '@/store/auth-store';
+import useAxiosPrivate from '@/hooks/use-axios-private';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/AlertDialog';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/Tooltip';
+import toast from 'react-hot-toast';
 
 interface StrategicProps {
   data: Strategic[] | null;
-  addUrl: string;
-  editUrl: string;
+  baseUrl: string;
 }
 
-const Strategic: FC<StrategicProps> = ({ data, addUrl, editUrl }) => {
+const Strategic: FC<StrategicProps> = ({ data, baseUrl }) => {
+  const axios = useAxiosPrivate();
+  const role = useAuthStore((state) => state.session?.role);
+
+  const deleteStrategic = async (id: string) => {
+    try {
+      await axios.delete(`/strategic/${id}`);
+      toast.success('Strategic deleted successfully!');
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    } catch (err) {
+      toast.error('Failed to delete strategic!');
+    }
+  };
+
   return (
     <div className='flex flex-col gap-2'>
       <div className='pb-4'>
@@ -25,21 +61,15 @@ const Strategic: FC<StrategicProps> = ({ data, addUrl, editUrl }) => {
             description='Startup detail information about Strategic.'
           />
           <div className='flex gap-4'>
-            <Link
-              href={addUrl + '/financial-report'}
-              className={cn(buttonVariants({ size: 'lg' }))}
-            >
-              Add new
-            </Link>
-            {data && data.length > 0 && (
+            {role === 'ADMIN' && (
               <Link
-                href={editUrl + '/financial-report'}
+                href={baseUrl + '/strategic/create'}
                 className={cn(
-                  buttonVariants({ size: 'lg' }),
-                  'bg-tertiary hover:bg-tertiary hover:opacity-90 w-full'
+                  buttonVariants({ size: 'sm' }),
+                  'bg-tertiary hover:bg-tertiary hover:opacity-90'
                 )}
               >
-                Edit
+                Add new
               </Link>
             )}
           </div>
@@ -62,6 +92,48 @@ const Strategic: FC<StrategicProps> = ({ data, addUrl, editUrl }) => {
                   <span className='text-lg font-semibold'>
                     {enumReplacer(item.business_point)}
                   </span>
+                  {role === 'ADMIN' && (
+                    <div className='flex flex-row gap-2'>
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <Link href={`${baseUrl}/strategic/${item.id}/edit`}>
+                              <Edit className='h-4 w-4' />
+                            </Link>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>Edit data</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+
+                      <AlertDialog>
+                        <AlertDialogTrigger>
+                          <Trash2 className='h-4 w-4' />
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                          <AlertDialogHeader>
+                            <AlertDialogTitle>
+                              Are you absolutely sure?
+                            </AlertDialogTitle>
+                            <AlertDialogDescription>
+                              Please be aware that this action is irreversible.
+                              Once completed, the data will be permanently
+                              erased and cannot be retrieved.
+                            </AlertDialogDescription>
+                          </AlertDialogHeader>
+                          <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction
+                              onClick={() => deleteStrategic(item.id)}
+                            >
+                              Continue
+                            </AlertDialogAction>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                    </div>
+                  )}
                 </div>
                 <p>{item.description}</p>
               </div>
