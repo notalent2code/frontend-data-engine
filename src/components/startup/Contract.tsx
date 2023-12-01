5a8989a9b702ef14ed889f3b17c8ae04b9b76a8a
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-12-02 02:00:07 +0700

    Add contract CRUD functionality

--- src/components/startup/Contract.tsx
+++ src/components/startup/Contract.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   BadgePercent,
   CalendarDays,
@@ -14,45 +16,99 @@ import { Heading } from '@/components/ui/Heading';
 import { Separator } from '@/components/ui/Separator';
 import { buttonVariants } from '@/components/ui/Button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/Card';
+import { useAuthStore } from '@/store/auth-store';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/AlertDialog';
+import useAxiosPrivate from '@/hooks/use-axios-private';
+import toast from 'react-hot-toast';
 
 interface ContractProps {
   data: Contract | null;
-  addUrl: string;
-  editUrl: string;
+  baseUrl: string;
 }
 
-const Contract: FC<ContractProps> = ({ data, addUrl, editUrl }) => {
+const Contract: FC<ContractProps> = ({ data, baseUrl }) => {
+  const axios = useAxiosPrivate();
+  const role = useAuthStore((state) => state.session?.role);
+
+  const deleteContract = async (id: string) => {
+    try {
+      await axios.delete(`/contract/${id}`);
+      toast.success('Contract deleted successfully!');
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    } catch (error: any) {
+      toast.error('Something went wrong!');
+    }
+  };
+
   return (
     <>
-      <div>
+      <div className='flex flex-col gap-2'>
         <div className='flex flex-col pt-16 lg:pt-0 lg:flex-row items-start lg:items-center justify-between'>
           <Heading
             title='Contract'
             description='Signed contract information.'
           />
-          {data ? (
-            <Link
-              href={editUrl + `/contract/${data.id}`}
-              className={cn(
-                buttonVariants({ size: 'lg' }),
-                'bg-tertiary hover:bg-tertiary hover:opacity-90'
+          {role === 'ADMIN' && (
+            <div className='flex flex-row gap-2'>
+              <Link
+                href={
+                  data
+                    ? `${baseUrl}/contract/${data.id}/edit`
+                    : `${baseUrl}/contract/create`
+                }
+                className={cn(
+                  buttonVariants({ size: 'sm' }),
+                  'bg-tertiary hover:bg-tertiary hover:opacity-90'
+                )}
+              >
+                {data ? 'Edit' : 'Add new'}
+              </Link>
+              {data && (
+                <div className={buttonVariants({ size: 'sm' })}>
+                  <AlertDialog>
+                    <AlertDialogTrigger>Delete</AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>
+                          Are you absolutely sure?
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                          Please be aware that this action is irreversible. Once
+                          completed, the data will be permanently erased and
+                          cannot be retrieved.
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction
+                          onClick={() => deleteContract(data.id)}
+                        >
+                          Continue
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </div>
               )}
-            >
-              Edit
-            </Link>
-          ) : (
-            <Link
-              href={addUrl + '/contract'}
-              className={cn(buttonVariants({ size: 'lg' }))}
-            >
-              Add new
-            </Link>
+            </div>
           )}
         </div>
         <Separator className='mt-4 lg:mt-0' />
       </div>
       {data ? (
-        <Card>
+        <Card className='my-4'>
           <CardHeader>
             <Card className='py-6 px-4 w-fit'>
               <CardTitle>PKS Number: {data.pks_number}</CardTitle>
@@ -71,7 +127,7 @@ const Contract: FC<ContractProps> = ({ data, addUrl, editUrl }) => {
                 {dayjs(data.closing_bak_date).format('D MMMM YYYY')}
               </Card>
               <Card className='flex flex-row items-center justify-start text-sm gap-2 p-4'>
-                <span className='font-bold'>Total Funding:</span>
+                <span className='font-bold'>Total Funding (IDR):</span>
                 <CircleDollarSign className='w-4 h-4' />
                 {formatCurrencyValue(parseInt(data.total_funding.toString()))}
               </Card>
@@ -81,7 +137,12 @@ const Contract: FC<ContractProps> = ({ data, addUrl, editUrl }) => {
               <Card className='flex flex-row items-center justify-start text-sm gap-2 p-4'>
                 <span className='font-bold'>CN Percentage:</span>
                 <BadgePercent className='w-4 h-4' />
-                <p>{parseFloat(data.convertible_note.toString()) * 100} %</p>
+                <p>
+                  {(parseFloat(data.convertible_note.toString()) * 100).toFixed(
+                    2
+                  )}{' '}
+                  %
+                </p>
               </Card>
               <Card className='flex flex-row items-center justify-start text-sm gap-2 p-4'>
                 <span className='font-bold'>CN Year:</span>
@@ -102,7 +163,9 @@ const Contract: FC<ContractProps> = ({ data, addUrl, editUrl }) => {
           </CardContent>
         </Card>
       ) : (
-        <p className='text-sm text-muted-foreground py-2'>No contract information found.</p>
+        <p className='text-sm text-muted-foreground py-2'>
+          No contract information found.
+        </p>
       )}
     </>
   );
