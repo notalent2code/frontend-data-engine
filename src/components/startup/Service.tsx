35bfe224b6e7a4e665a8fae098916e41c47989c1
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-12-03 05:14:16 +0700

    Refactor Service component to include add and delete functionality

--- src/components/startup/Service.tsx
+++ src/components/startup/Service.tsx
@@ -1,44 +1,74 @@
+'use client';
+
 import { FC } from 'react';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { Service } from '@prisma/client';
-import { BadgePercent } from 'lucide-react';
+import { BadgePercent, Edit, Trash2 } from 'lucide-react';
 import { Card } from '@/components/ui/Card';
 import { Heading } from '@/components/ui/Heading';
 import { Separator } from '@/components/ui/Separator';
 import { buttonVariants } from '@/components/ui/Button';
+import useAxiosPrivate from '@/hooks/use-axios-private';
+import { useAuthStore } from '@/store/auth-store';
+import toast from 'react-hot-toast';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/AlertDialog';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/Tooltip';
 
 interface ServiceProps {
   data: Service[] | null;
-  addUrl: string;
-  editUrl: string;
+  baseUrl: string;
 }
 
-const Service: FC<ServiceProps> = ({ data, addUrl, editUrl }) => {
+const Service: FC<ServiceProps> = ({ data, baseUrl }) => {
+  const axios = useAxiosPrivate();
+  const role = useAuthStore((state) => state.session?.role);
+
+  const deleteService = async (id: string) => {
+    try {
+      await axios.delete(`/service/${id}`);
+      toast.success('Service deleted successfully!');
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    } catch (err) {
+      toast.error('Failed to delete service!');
+    }
+  };
+
   return (
     <div className='flex flex-col gap-2'>
-      <div className='py-4'>
+      <div className='pt-4'>
         <div className='flex flex-col pt-16 lg:pt-0 lg:flex-row items-start lg:items-center justify-between'>
           <Heading
             title='Service'
             description="Startup detail information about services they provides and it's revenue percentage."
           />
           <div className='flex gap-4'>
-            <Link
-              href={addUrl + '/financial-report'}
-              className={cn(buttonVariants({ size: 'lg' }))}
-            >
-              Add new
-            </Link>
-            {data && data.length > 0 && (
+            {role === 'ADMIN' && (
               <Link
-                href={editUrl + '/financial-report'}
+                href={baseUrl + '/service/create'}
                 className={cn(
-                  buttonVariants({ size: 'lg' }),
-                  'bg-tertiary hover:bg-tertiary hover:opacity-90 w-full'
+                  buttonVariants({ size: 'sm' }),
+                  'bg-tertiary hover:bg-tertiary hover:opacity-90'
                 )}
               >
-                Edit
+                Add new
               </Link>
             )}
           </div>
@@ -46,15 +76,57 @@ const Service: FC<ServiceProps> = ({ data, addUrl, editUrl }) => {
         <Separator className='mt-4 lg:mt-0' />
       </div>
       {data && data.length > 0 ? (
-        <div className='flex flex-row gap-4'>
-          {data.map((service) => (
+        <div className='flex flex-row gap-4 pt-4'>
+          {data.map((item) => (
             <Card
-              key={service.id}
+              key={item.id}
               className='flex flex-row items-center justify-start text-sm gap-2 p-4'
             >
-              <span className='font-bold'>{service.title}</span>
+              <span className='font-bold'>{item.title}</span>
               <BadgePercent className='w-4 h-4' />
-              {service.revenue_percentage} %
+              {item.revenue_percentage} %
+              {role === 'ADMIN' && (
+                <div className='flex flex-row gap-2'>
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <Link href={`${baseUrl}/service/${item.id}/edit`}>
+                          <Edit className='h-4 w-4' />
+                        </Link>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Edit data</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+
+                  <AlertDialog>
+                    <AlertDialogTrigger>
+                      <Trash2 className='h-4 w-4' />
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>
+                          Are you absolutely sure?
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                          Please be aware that this action is irreversible. Once
+                          completed, the data will be permanently erased and
+                          cannot be retrieved.
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction
+                          onClick={() => deleteService(item.id)}
+                        >
+                          Continue
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </div>
+              )}
             </Card>
           ))}
         </div>
