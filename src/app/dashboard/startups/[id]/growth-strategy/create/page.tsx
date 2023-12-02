b0f9cff759d715f94938907085bf8aeb30e05660
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2023-12-03 05:15:51 +0700

    Add growth strategy form component to create page

---
+++ src/app/dashboard/startups/[id]/growth-strategy/create/page.tsx
@@ -0,0 +1,9 @@
+import GrowthStrategyForm from "@/components/startup/forms/GrowthStrategyForm"
+
+const Page = () => {
+  return (
+    <GrowthStrategyForm variant='create' />
+  )
+}
+
+export default Page
